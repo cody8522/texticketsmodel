2319af2544867ac5873052187c125b4e94f33557
cody8522 <64490876+cody8522@users.noreply.github.com>
2022-06-02 13:52:56 -0500

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -16,12 +16,10 @@ chartcols = ['sp_followers',
 	 'sp_playlist_total_reach',
 	 'cm_artist_rank','cm_artist_score','facebook_followers','ins_followers']
 
-df = pd.read_csv('BIG-SHOWS-RAW.csv')
-	st.write(df.head())
-
-
 @st.experimental_memo(suppress_st_warning = True)
 def csv_load():
+	df = pd.read_csv('BIG-SHOWS-RAW.csv')
+	st.write(df.head())
 	event = pd.read_csv('Old Shows - Old Shows (1).csv')
 	venue_info = pd.read_csv('Venue Information.csv')
 	df = df.merge(venue_info[['Name','Adjusted Capacity']], on='Name').drop('Unnamed: 0',axis=1)
